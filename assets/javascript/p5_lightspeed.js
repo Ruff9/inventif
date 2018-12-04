cc9053b3f4a2ae2a9a31a7d106a0b3d883b221e9
Remy Maucourt <remy.maucourt@yahoo.fr>
2018-12-04 19:26:57 +0100

    refactoring lightspeed

--- assets/javascript/p5_lightspeed.js
+++ assets/javascript/p5_lightspeed.js
@@ -3,6 +3,10 @@ var warpStart = 60;
 var warpEnd = 240;
 var time = 0;
 
+var initialSpeed = 1.0005;
+var speed1 = 1.025;
+var speed2 = 1.06;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -10,7 +14,8 @@ function setup() {
     stars[i] = {
       posX1: Math.random() * width - width/2,
       posY1: Math.random() * height - height/2,
-      size: map(Math.random(), 0, 1, 1, 3)
+      size: map(Math.random(), 0, 1, 1, 3),
+      color: Math.floor(Math.random() * 155) + 150
     }
 
     stars[i].posX2 = stars[i].posX1;
@@ -22,64 +27,70 @@ function setup() {
 function draw() {
   background(0,0,20);
 
-  fill(255);
-  stroke(255);
-
   translate(width/2, height/2);
   // translate(mouseX, mouseY);
 
-  var initialSpeed = 1.0005;
-  var speed1 = 1.04;
-  var speed2 = 1.06;
-
   stars.forEach(function(star) {
+    stroke(star.color);
     strokeWeight(star.size);
+
     line(star.posX1, star.posY1, star.posX2, star.posY2);
 
     if (time < warpStart) {
-      star.posX1 = initialSpeed*star.posX1;
-      star.posY1 = initialSpeed*star.posY1;
-      star.posX2 = initialSpeed*star.posX2;
-      star.posY2 = initialSpeed*star.posY2;
+      initial(star)
     } else if (time > warpEnd) {
-      if (Math.abs(star.posX1) < Math.abs(star.posX2)) {
-        star.posX1 = speed1*star.posX1;
-      } else {
-        star.posX1 = initialSpeed*star.posX2;
-      }
-
-      if (Math.abs(star.posY1) < Math.abs(star.posY2)) {
-        star.posY1 = speed1*star.posY1;
-      } else {
-        star.posY1 = initialSpeed*star.posY2;
-      }
-
-      star.posX2 = initialSpeed*star.posX2;
-      star.posY2 = initialSpeed*star.posY2;
-
-      if (Math.abs(star.posX1) > width/2 || Math.abs(star.posY1) > height/2) {
-        star.posX1 = (Math.random() * width - width/2);
-        star.posY1 = (Math.random() * height - height/2);
-        star.posX2 = star.posX1;
-        star.posY2 = star.posY1;
-      }
+      slowing(star);
     } else {
-      star.posX1 = speed1*star.posX1;
-      star.posY1 = speed1*star.posY1;
-      star.posX2 = speed2*star.posX2;
-      star.posY2 = speed2*star.posY2;
-
-      if (Math.abs(star.posX1) > width/2 || Math.abs(star.posY1) > height/2) {
-        star.posX1 = (Math.random() * width - width/2)/2;
-        star.posY1 = (Math.random() * height - height/2)/2;
-        star.posX2 = star.posX1;
-        star.posY2 = star.posY1;
-      }
+      accelerating(star)
     }
-
   });
 
   time++;
 
   if (time == 500) { time = 0; }
+}
+
+function initial(star) {
+  star.posX1 = initialSpeed*star.posX1;
+  star.posY1 = initialSpeed*star.posY1;
+  star.posX2 = initialSpeed*star.posX2;
+  star.posY2 = initialSpeed*star.posY2;
+}
+
+function accelerating(star) {
+  star.posX1 = speed1*star.posX1;
+  star.posY1 = speed1*star.posY1;
+  star.posX2 = speed2*star.posX2;
+  star.posY2 = speed2*star.posY2;
+
+  if (Math.abs(star.posX1) > width/2 || Math.abs(star.posY1) > height/2) {
+    star.posX1 = (Math.random() * width - width/2)/2;
+    star.posY1 = (Math.random() * height - height/2)/2;
+    star.posX2 = star.posX1;
+    star.posY2 = star.posY1;
+  }
+}
+
+function slowing(star) {
+  if (Math.abs(star.posX1) < Math.abs(star.posX2)) {
+    star.posX1 = speed1*star.posX1;
+  } else {
+    star.posX1 = initialSpeed*star.posX2;
+  }
+
+  if (Math.abs(star.posY1) < Math.abs(star.posY2)) {
+    star.posY1 = speed1*star.posY1;
+  } else {
+    star.posY1 = initialSpeed*star.posY2;
+  }
+
+  star.posX2 = initialSpeed*star.posX2;
+  star.posY2 = initialSpeed*star.posY2;
+
+  if (Math.abs(star.posX1) > width/2 || Math.abs(star.posY1) > height/2) {
+    star.posX1 = (Math.random() * width - width/2);
+    star.posY1 = (Math.random() * height - height/2);
+    star.posX2 = star.posX1;
+    star.posY2 = star.posY1;
+  }
 }
