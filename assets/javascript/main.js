57a75e3ec42e14d1f4e35fddf9640e02d50c8f5f
Rémy Maucourt <remy.maucourt@yahoo.fr>
2025-11-02 11:36:48 +0100

    rework newsletter page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,17 +1,5 @@
-// from https://codepen.io/KanatSahanov/pen/raGXXa
-
 (function() {
-  function ready(fn) {
-    if (document.readyState != 'loading'){
-      fn();
-    } else {
-      document.addEventListener('DOMContentLoaded', fn);
-    }
-  }
-
   randomColor();
-  ready(openModal);
-  ready(closeModal);
 }());
 
 function randomColor() {
@@ -28,38 +16,4 @@ function randomColor() {
   var randomColor = colorList[Math.floor(Math.random() * colorList.length)];
 
   document.documentElement.style.setProperty(`--primary-color`, randomColor);
-}
-
-function openModal() {
-  var modalTrigger = document.getElementsByClassName('jsModalTrigger');
-
-  for(var i = 0; i < modalTrigger.length; i++) {
-    modalTrigger[i].onclick = function() {
-      var target = this.getAttribute('href').substr(1);
-      var modalWindow = document.getElementById(target);
-
-      modalWindow.classList ? modalWindow.classList.add('open') : modalWindow.className += ' ' + 'open';
-    }
-  }
-}
-
-function closeModal(){
-  var closeButton = document.getElementsByClassName('jsModalClose');
-  var closeOverlay = document.getElementsByClassName('jsOverlay');
-
-  for(var i = 0; i < closeButton.length; i++) {
-    closeButton[i].onclick = function() {
-      var modalWindow = this.parentNode.parentNode;
-
-      modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
-  }
-
-  for(var i = 0; i < closeOverlay.length; i++) {
-    closeOverlay[i].onclick = function() {
-      var modalWindow = this.parentNode;
-
-      modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
-  }
 }
